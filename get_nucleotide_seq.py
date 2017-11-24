f75ef59836df84a9273526c552f76770a07464ce
Steven Weaver <sweaver@temple.edu>
2017-11-24 14:36:00 -0500

    adding nucleotide seq getter

---
+++ get_nucleotide_seq.py
@@ -0,0 +1,24 @@
+from Bio import SeqIO
+from Bio import Entrez
+
+Entrez.email = "steven@stevenweaer.org"
+output_filename = "opsins.fas"
+
+def get_nuc_from_prot_accession(id):
+	handle = Entrez.efetch(db="protein", id=id, rettype="gb", retmode="text")
+	record = SeqIO.read(handle, "genbank")
+
+	# Get db_source
+	source = record.annotations["db_source"]
+
+	handle = Entrez.efetch(db="nucleotide", id=source,rettype="gb", retmode="text")
+	record = next(SeqIO.parse(handle, "genbank"))
+	return record
+
+# add sequence to concatenated FASTA file
+
+with open('accs_two', 'r') as f:
+	lines = f.readlines()
+	recs = map(lambda x : get_nuc_from_prot_accession(x), lines)
+
+SeqIO.write(recs, output_filename, "fasta")
